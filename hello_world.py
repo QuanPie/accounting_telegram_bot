b19f06f89df73d1784e679b4f9e0010e6c5ad444
長權 <changquan@changquandeMacBook-Air.local>
2022-02-03 21:27:37 +0800

    first bot test

--- hello_world.py
+++ hello_world.py
@@ -2,6 +2,8 @@
 # bot = telegram.Bot(token='5228328595:AAH35Qe5QSCyHKgJDNmWGv6JRrsB5CXjdaI')
 # print(bot.get_me())
 
+from telegram.ext import InlineQueryHandler
+from telegram import InlineQueryResultArticle, InputTextMessageContent
 from telegram.ext import MessageHandler, Filters
 from telegram.ext import CommandHandler
 from telegram.ext import CallbackContext
@@ -32,8 +34,6 @@ hello_handler = CommandHandler('hello', hello)
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(hello_handler)
 
-updater.start_polling()
-
 
 def echo(update: Update, context: CallbackContext):
     context.bot.send_message(
@@ -42,3 +42,43 @@ def echo(update: Update, context: CallbackContext):
 
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
+
+
+def caps(update: Update, context: CallbackContext):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
+
+
+def inline_caps(update: Update, context: CallbackContext):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = []
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    context.bot.answer_inline_query(update.inline_query.id, results)
+
+
+inline_caps_handler = InlineQueryHandler(inline_caps)
+dispatcher.add_handler(inline_caps_handler)
+
+
+def unknown(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Sorry, I didn't understand that command.")
+
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
+
+updater.start_polling()
