4b73efc1542a11d4e4b534340adb28fb73ff61c8
cream314159 <ss1041310413@gmail.com>
2022-03-21 16:11:36 +0800

    ConversationHandler_unfinish

--- hello_world.py
+++ hello_world.py
@@ -3,12 +3,23 @@
 # This program is dedicated to the public domain under the CC0 license.
 
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext, CallbackQueryHandler, Filters
+from turtle import update
 from telegram import InlineKeyboardButton, ReplyKeyboardMarkup, Update, InlineKeyboardMarkup
+from telegram.ext import(
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    CallbackContext,
+    CallbackQueryHandler,
+    Filters,
+    ConversationHandler,
+)
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+CHOICE = range(0)
 
 
 def start(update: Update, context: CallbackContext):
@@ -59,8 +70,24 @@ def version(update: Update, context: CallbackContext):
     )
 
 
-def expense(update: Update, context: CallbackContext):
+def income_choice(update: Update, context: CallbackContext):
+    keyboard = [
+        [
+            InlineKeyboardButton("Salary", callback_data="salary"),
+            InlineKeyboardButton("Trade", callback_data="trade"),
+            InlineKeyboardButton("Bonus", callback_data="bonus"),
+        ],
+        [
+            InlineKeyboardButton("Bonus", callback_data="bonus"),
+            InlineKeyboardButton("Subsidize", callback_data="subsidize"),
+            InlineKeyboardButton("Others", callback_data="others"),
+        ],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
+
+def expense_choice(update: Update, context: CallbackContext):
     keyboard = [
         [
             InlineKeyboardButton("Food", callback_data="food"),
@@ -74,32 +101,59 @@ def expense(update: Update, context: CallbackContext):
             InlineKeyboardButton("Others", callback_data="others"),
         ],
     ]
-
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
+    return CHOICE
 
-def button(update: Update, context: CallbackContext) -> None:
+
+def how_much(update: Update, context: CallbackContext) -> None:
     """Parses the CallbackQuery and updates the message text."""
+    """to-do 判斷上一步選擇是income或是expense 這邊文字要get或cost"""
     query = update.callback_query
     # CallbackQueries need to be answered, even if no notification to the user is needed
     # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
     query.answer()
 
-    query.edit_message_text(
-        text=f"How much did you cost on {query.data}? (enter the number)")
+    if query is not None:
+        query.edit_message_text(
+            text=f"How much did you cost on {query.data}? (enter the number)")
+
+
+def end():
+    update.message.reply_text(
+        'Thank You, if you want open the menu, just type /start')
+    return ConversationHandler.END
 
 
 def main():
     # creat updater and pass it your bot's token
     updater = Updater(token='5228328595:AAH35Qe5QSCyHKgJDNmWGv6JRrsB5CXjdaI')
 
+    expense_handler = ConversationHandler(
+        entry_points=[MessageHandler(
+            Filters.regex('💸 Expense'), expense_choice)],
+        states=[
+            CHOICE:[
+                CallbackQueryHandler(how_much)
+            ]
+        ],
+        fallbacks=[
+            CommandHandler(end, end)
+        ]
+    )
+
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CommandHandler('help', help))
     updater.dispatcher.add_handler(CommandHandler('version', version))
-    updater.dispatcher.add_handler(
-        MessageHandler(Filters.regex('💸 Expense'), expense))
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+
+    # updater.dispatcher.add_handler(MessageHandler(
+    #     Filters.regex('💸 Expense'), expense_choice))
+    updater.dispatcher.add_handler(expense_handler)
+    updater.dispatcher.add_handler(MessageHandler(
+        Filters.regex('💰 Income'), income_choice))
+    updater.dispatcher.add_handler(CallbackQueryHandler(how_much))
+
     # start the bot
     updater.start_polling()
 
